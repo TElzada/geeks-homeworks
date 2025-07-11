ea67102fe40d773e5e1a3afd7d24e920028a6167
TElzada <turdubekovaelzada@gmail.com>
2025-07-11 22:21:51 +0600

    did all the tasks

--- src/lesson_2_1.java
+++ src/lesson_2_1.java
@@ -1,8 +1,30 @@
+import java.util.Random;
+
 public class lesson_2_1 {
-    public String permission{
-
+    public static String permission(int age, int temperature) {
+        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
+            return "Можно идти гулять";
+        } else if (age < 20 && temperature >= 0 && temperature <= 28) {
+            return "Можно идти гулять";
+        } else if (age > 45 && temperature >= -10 && temperature <= 25) {
+            return "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
+        }
     }
-    public void main (String[]  args){
 
+
+    public static int generateRandomAge() {
+        Random random = new Random();
+        return random.nextInt(101); // Включила возраст от 0 до 100 включительно
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(permission(generateRandomAge(), 45));
+        System.out.println(permission(generateRandomAge(), -15));
+        System.out.println(permission(generateRandomAge(), 22));
+        System.out.println(permission(generateRandomAge(), 37));
+        System.out.println(permission(generateRandomAge(), -5));
     }
 }
